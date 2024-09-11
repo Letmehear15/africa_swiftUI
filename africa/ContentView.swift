ce2f1d26b7fcda29ad3743ea0771300fc992b9c8
alex <aleksandr.nesterov@notino.com>
2024-09-11 12:56:41 +0200

    Initial Commit

---
+++ africa/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  africa
+//
+//  Created by Aleksandr Nesterov on 11.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ africa/africaApp.swift
@@ -0,0 +1,17 @@
+//
+//  africaApp.swift
+//  africa
+//
+//  Created by Aleksandr Nesterov on 11.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct africaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
